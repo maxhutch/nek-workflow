f5fb5d297192283aaf99a79864930ca0dba97afc
Max Hutchinson <maxhutch@gmail.com>
2015-02-06 10:03:22 -0600

    Added CLI (stolen from maxhutch/nek-analyze).

--- post_proc.py
+++ post_proc.py
@@ -2,55 +2,43 @@
 
 #!/home/maxhutch/anaconda3/bin/python
 
-arch_end = "acherry#hpss_test/~/pub/"
-outp_end = "maxhutch#alpha-admin/pub/"
-home_end = "maxhutch#edoras/home/maxhutch/science/RTI/"
-home = "/home/maxhutch/science/RTI/"
-
-#source = "fingers/hemisphere/hemi_finger"
-#source = "fingers/hemisphere_wide/hemi_wide"
-source = "fingers/cone/cone_finger"
+from ui import command_line_ui
+args = command_line_ui()
 
 from os.path import dirname, basename
-experiment = dirname(source)
-run = basename(source)
-start = 4
-end   = 4
-nodes = 1
-
-enable_archive = False
-enable_upload = True
+experiment = dirname(args.name)
+run = basename(args.name)
 
 import json
-with open("{:s}.json".format(home+source), "r") as f:
+with open("{:s}.json".format(args.root+args.name), "r") as f:
   params = json.load(f)
 
 from names import get_fname
-fname = get_fname(home+source, 0, end, params)
+fname = get_fname(args.root+args.name, 0, args.frame_end, params)
 from os.path import exists
-new_source = exists(fname)
+new_source = not exists(fname)
 
-if new_source:
- if enable_archive:
+if not new_source:
+ if args.archive:
   print("Found {:s}, archiving".format(fname))
   from globus import archive
-  archive(arch_end, home_end, source, start, end, params)
+  archive(args.archive_end, args.home_end, args.name, args.frame, args.frame_end, params)
 else:
   print("Not Found {:s}, recovering".format(fname))
   from globus import recover
-  recover(arch_end, home_end, source, start, end, params)
+  recover(args.archive_end, args.home_end, args.name, args.frame, args.frame_end, params)
 
 # queue processing job
-if nodes != 0:
+if args.nodes != 0:
   from analyze import process
-  process(home+source, start, end, nodes)
+  process(args.root+args.name, args.frame, args.frame_end, args.nodes)
 
 # setup upload
 from globus import upload_results
-if enable_upload:
-  upload_results(home_end, outp_end, source)
+if args.upload:
+  upload_results(args.home_end, args.output_end, args.name)
 
 # setup archive
-if enable_upload:
-  upload_results(home_end, arch_end, source)
+if args.upload:
+  upload_results(args.home_end, args.archive_end, args.name)
 
---
+++ ui.py
@@ -0,0 +1,94 @@
+"""
+User interfaces for the nek-workflow script
+
+Currently, there is only a command line interface
+"""
+
+
+def command_line_ui():
+  """
+  Command line interface for nek-workflow
+
+  Uses python's ArgumentParser to read the command line and then creates
+  shortcuts for common argument combinations
+  """
+
+  # grab defaults from config files
+  from os.path import exists, expanduser, join
+  import json
+  defaults = {
+               'start' : 1,
+               'nodes' : 1,
+               'enable_archive' : False,
+               'enable_upload'  : False,
+               'arch_end' : "acherry#hpss_test/~/pub/",
+               'outp_end' : "maxhutch#alpha-admin/pub/",
+               'home_end' : "maxhutch#edoras/home/maxhutch/science/RTI/",
+               'foo'      : "bar"
+             }
+  if exists(join(expanduser("~"), ".nek-workflow.json")):
+    with open(join(expanduser("~"), ".nek-workflow.json")) as f:
+      defaults.update(json.load(f))
+
+  # Define arguments
+  from argparse import ArgumentParser
+  p = ArgumentParser()
+  p.add_argument("name",                 
+                 help="Name and path of Nek output files")
+  p.add_argument("-a", "--archive_end", default=defaults["arch_end"],
+                 help="Archive endpoint")
+  p.add_argument("-m", "--home_end", default=defaults["home_end"],
+                 help="Home endpoint")
+  p.add_argument("-o", "--output_end", default=defaults["outp_end"],
+                 help="Output endpoint")
+  p.add_argument("-f",  "--frame", type=int, default=1, 
+                 help="[Starting] Frame number")
+  p.add_argument("-e",  "--frame_end", type=int, default=-1,   
+                 help="Ending frame number")
+  p.add_argument("--archive", default=defaults["enable_archive"],
+                 help="Archive raw")
+  p.add_argument("--upload", default=defaults["enable_upload"],
+                 help="Upload results")
+  p.add_argument("-n", "--nodes", default=1,
+                 help="Number of nodes to run on")
+
+  """ 
+  p.add_argument("-s",  "--slice", action="store_true",
+                 help="Display slice")
+  p.add_argument("-c",  "--contour", action="store_true",     
+                 help="Display contour")
+  p.add_argument("-n",  "--ninterp", type=float, default = 1.,
+                 help="Interpolating order")
+  p.add_argument("-z",  "--mixing_zone", action="store_true",
+                 help="Compute mixing zone width")
+  p.add_argument("-m",  "--mixing_cdf", action="store_true",
+                 help="Plot CDF of box temps")
+  p.add_argument("-F",  "--Fourier", action="store_true",
+                 help="Plot Fourier spectrum in x-y")
+  p.add_argument("-b",  "--boxes", action="store_true",
+                 help="Compute box covering numbers")
+  p.add_argument("-nb", "--block", type=int, default=65536,
+                 help="Number of elements to process at a time")
+  p.add_argument("-nt", "--thread", type=int, default=1,
+                 help="Number of threads to spawn")
+  p.add_argument("-d",  "--display", action="store_true", default=False,  
+                 help="Display plots with X")
+  p.add_argument("-p",  "--parallel", action="store_true", default=False,
+                 help="Use parallel map (IPython)")
+  p.add_argument(       "--series", action="store_true", default=False,
+                 help="Apply time-series analyses")
+  p.add_argument("--mapreduce", default=defaults["mapreduce"],
+                 help="Module containing Map and Reduce implementations")
+  p.add_argument("--post", default=defaults["post"],
+                 help="Module containing post_frame and post_series")
+  p.add_argument("-v",  "--verbose", action="store_true", default=False,
+                 help="Should I be really verbose, that is: wordy?")
+  """
+ 
+  # Load the arguments
+  args = p.parse_args()
+  if args.frame_end == -1:
+      args.frame_end = args.frame
+  args.root = "/" + args.home_end.partition("/")[2]
+  
+  return args
